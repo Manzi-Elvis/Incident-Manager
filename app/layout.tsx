d740432ff679ef52a007a48261e001c719d657a9
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2026-02-21 18:49:22 +0200

    ...

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
-import type { Metadata } from 'next'
 "use client"
+import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/next'
 import { AuthProvider } from '@/lib/context/auth-context'
